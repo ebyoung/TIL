b161d5f29021c132f10f3adab400a09464f0c260
ebyoung <ebyoung5183@naver.com>
2022-01-21 22:56:11 +0900

    solve algorithm problem

---
+++ A1g_study/code/string.py
@@ -0,0 +1,20 @@
+# 1316
+# n = int(input())
+# count = 0
+# for _ in range(n):
+#     word = input()
+#     prev_char = word[0]
+#     already_used = []
+#     for char in word:
+#         if prev_char == char:
+#             continue
+#         else:
+#             if char in already_used:
+#                 break
+#             else:
+#                 already_used.append(prev_char)
+#                 prev_char = char
+#     else:
+#         count += 1
+
+# print(count)
