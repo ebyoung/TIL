23b5291115ea950aefef8bbb310cb11c4e09f77d
ebyoung <ebyoung5183@naver.com>
2022-01-25 21:24:13 +0900

    0126 alg study

---
+++ A1g_study/personal/recursive.py
@@ -0,0 +1,37 @@
+# 10872
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+    
+#     return n * factorial(n-1)
+
+# n = int(input())
+# print(factorial(n))
+
+
+# 10870
+# def fib(n):
+#     if n <= 1:
+#         return n
+    
+#     return fib(n-1) + fib(n-2)
+
+# n = int(input())
+# print(fib(n))
+
+
+# 2447 다시
+# 2차원 배열 활용 -> *:1
+# def print_star(n):
+#     pass
+
+# n = int(input())
+# print(print_star(n))
+
+
+# 11729 다시
+# def hanoi(n, start, end):
+#     pass
+
+# n = int(input())
+# print(hanoi(n, 1, 3))
