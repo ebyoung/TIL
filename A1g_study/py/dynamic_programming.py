3afde20a8cb8843f10eea0f6afb2bd5bd54745de
ebyoung <ebyoung5183@naver.com>
2022-02-07 23:17:34 +0900

    0208 alg study

--- A1g_study/py/dynamic_programming.py
+++ A1g_study/py/dynamic_programming.py
@@ -17,18 +17,19 @@
 #     print(*fib_01(int(sys.stdin.readline())))
 
 # bottom_up
-import sys
-n = int(sys.stdin.readline())
+# import sys
+# n = int(sys.stdin.readline())
 
-dp = [[1, 0], [0, 1]]
+# dp = [[1, 0], [0, 1]]
 
-def fib_01(num):
-    if num >= len(dp):
-        for i in range(len(dp), num+1):
-            next_01 = [dp[i-2][0] + dp[i-1][0], dp[i-2][1]+ dp[i-1][1]]
-            dp.append(next_01)
+# def fib_01(num):
+#     if num >= len(dp):
+#         for i in range(len(dp), num+1):
+#             next_01 = [dp[i-2][0] + dp[i-1][0], dp[i-2][1]+ dp[i-1][1]]
+#             dp.append(next_01)
 
-    return dp[num]
+#     return dp[num]
 
-for _ in range(n):
-    print(*fib_01(int(sys.stdin.readline())))
+# for _ in range(n):
+#     print(*fib_01(int(sys.stdin.readline())))
+
