afa12187c5d177babba7a195d18829e7895c9965
ebyoung <ebyoung5183@naver.com>
2022-02-08 23:31:15 +0900

    0209 alg study

--- A1g_study/py/dynamic_programming.py
+++ A1g_study/py/dynamic_programming.py
@@ -33,3 +33,36 @@
 # for _ in range(n):
 #     print(*fib_01(int(sys.stdin.readline())))
 
+
+# 9184
+# import sys
+
+# w_dict = {}
+# def w(a, b, c):
+#     if w_dict.get((a, b, c)):
+#         return w_dict.get((a, b, c))
+
+#     if (a <= 0) or (b <= 0) or (c <= 0):
+#         w_dict[(a, b, c)] = 1
+#         return 1
+
+#     elif (a > 20) or (b > 20) or (c > 20):
+#         result = w(20, 20, 20)
+#         w_dict[(a, b, c)] = result
+#         return result
+
+#     elif (a < b) and (b < c):
+#         result = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+#         w_dict[(a, b, c)] = result
+#         return result
+    
+#     else:
+#         result = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+#         w_dict[(a, b, c)] = result
+#         return result
+
+# while True:
+#     a, b, c = map(int, sys.stdin.readline().split())
+#     if (a == -1) and (b == -1) and (c == -1):
+#         break
+#     print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
