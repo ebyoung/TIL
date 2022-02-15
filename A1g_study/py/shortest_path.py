0e92057047449fa2f87e3a77ec02f363047df404
ebyoung <ebyoung5183@naver.com>
2022-02-15 22:46:33 +0900

    0216 alg study

---
+++ A1g_study/py/shortest_path.py
@@ -0,0 +1,37 @@
+# 1753
+import sys
+
+
+INF = int(1e9)
+n, m = map(int, sys.stdin.readline().split())
+start = int(sys.stdin.readline())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a].append((b, c))
+
+import heapq
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print('INF')
+    else:
+        print(distance[i])
