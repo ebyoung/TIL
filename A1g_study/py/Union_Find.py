d961159e3df438fda45d3c0045d79e3a576cd3b9
ebyoung <ebyoung5183@naver.com>
2022-02-22 00:36:54 +0900

    Solve alg problem

--- A1g_study/py/Union_Find.py
+++ A1g_study/py/Union_Find.py
@@ -1,10 +1,11 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return parent[x]  # 부모 테이블 값을 루트 노트로 갱신해 버려서 효율적으로 탐색(경로 압축)
-
-
-#     return x : 매번 루트 노드를 찾기 위해 재귀적으로 거슬러 올라가야 함
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 
 def union_parent(parent, a, b):
@@ -16,20 +17,19 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 
-v, e = map(int, input().split())
-parent = [0] * (v + 1)
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+for i in range(1, n+1):
+    parent[i] = i
 
 # 유니온 연산 수행
-for i in range(e):
-    a, b = map(int, input().split())
-    union_parent(parent, a, b)
-
-print('각 원소가 속한 집합: ', end='')
-for i in range(1, v + 1):
-    print(find_parent(parent, i), end=' ')
-
-print()
-
-print('부모 테이블: ', end='')
-for i in range(1, v + 1):
-    print(parent[i], end=' ')
+for i in range(m):
+    p, a, b = map(int, input().split())
+    if p:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
+    else:
+        union_parent(parent, a, b)
