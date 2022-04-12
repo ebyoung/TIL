d0dd56c0d6783673102e4dd30c8658740cfef71d
ebyoung <ebyoung5183@naver.com>
2022-04-12 23:41:29 +0900

    0412 alg study

--- A1g_study/py/dynamic_programming.py
+++ A1g_study/py/dynamic_programming.py
@@ -216,3 +216,18 @@ for i in range(2, 10**6+1):
         break
 print(dp[x])
 '''
+
+# 2156
+''''''
+n = int(input())
+data = [int(input()) for _ in range(n)]
+dp = [0] * n
+dp[0] = data[0]
+if n > 1:
+    dp[1] = dp[0] + data[1]
+if n > 2:
+    dp[2] = max(dp[0] + data[2], data[1] + data[2], dp[1])
+for i in range(3, n):
+    dp[i] = max(dp[i-3] + data[i] + data[i-1], dp[i-2] + data[i], dp[i-1])
+
+print(dp[-1])
